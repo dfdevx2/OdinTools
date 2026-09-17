0ab291d0944fa0462025d48a68b2e761e2f8f682
dfdevx2 <viniciuseugenio47@gmail.com>
2026-09-17 23:49:10 +0100

    fix: remove configuracao de modulo do build.gradle raiz

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,70 +1,25 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
 plugins {
-    alias(libs.plugins.com.android.application)
-    alias(libs.plugins.org.jetbrains.kotlin.android)
-    alias(libs.plugins.compose.compiler) // Novo compilador K2
-    alias(libs.plugins.com.google.dagger.hilt.android)
-    alias(libs.plugins.com.google.devtools.ksp)
-    alias(libs.plugins.androidx.room)
+    alias(libs.plugins.com.android.application) apply false
+    alias(libs.plugins.org.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.compose.compiler) apply false
+    alias(libs.plugins.com.google.dagger.hilt.android) apply false
+    alias(libs.plugins.com.google.devtools.ksp) apply false
+    alias(libs.plugins.androidx.room) apply false
+    alias(libs.plugins.com.github.ben.manes.versions)
+    alias(libs.plugins.nl.littlerobots.version.catalog.update)
 }
 
-android {
-    namespace = "de.langerhans.odintools"
-    compileSdk = 34
-
-    defaultConfig {
-        applicationId = "de.langerhans.odintools"
-        minSdk = 33
-        targetSdk = 34
-        versionCode = 11
-        versionName = "1.3.1"
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            initWith(buildTypes.getByName("debug"))
-            isDebuggable = false
-            isMinifyEnabled = true
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-        }
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-
-    buildFeatures {
-        compose = true
-        buildConfig = true
-    }
-
-    packaging {
-        resources { excludes += "/META-INF/{AL2.0,LGPL2.1}" }
-    }
+fun isNonStable(version: String): Boolean {
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    val isStable = stableKeyword || regex.matches(version)
+    return isStable.not()
 }
 
-room {
-    schemaDirectory("$projectDir/schemas")
-}
-
-dependencies {
-    val composeBom = platform(libs.androidx.compose.bom)
-    implementation(composeBom)
-    androidTestImplementation(composeBom)
-    debugImplementation(composeBom)
-
-    implementation(libs.bundles.app)
-    debugImplementation(libs.bundles.appDebug)
-    annotationProcessor(libs.bundles.appAnnotationProcessor)
-    ksp(libs.bundles.appKsp)
-    testImplementation(libs.bundles.appUnitTest)
-    androidTestImplementation(libs.bundles.appAndroidTest)
-
-    implementation(libs.com.google.dagger.hilt.android)
-    ksp(libs.com.google.dagger.hilt.android.compiler)
+tasks.withType<DependencyUpdatesTask> {
+    rejectVersionIf {
+        isNonStable(candidate.version) && !isNonStable(currentVersion)
+    }
 }
